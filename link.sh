8971d15f0bb0d7c0a65d72037b3f3148d5251b27
Wilfred Godfrey <w.j.godfrey@gmail.com>
2016-01-21 09:13:28 +1300

    Add gitignore to dotfiles.

--- link.sh
+++ link.sh
@@ -6,9 +6,9 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc vim nethackrc gitconfig"   		  # list of files/folders to symlink in homedir
+dir=~/dotfiles                                      # dotfiles directory
+olddir=~/dotfiles_old                               # old dotfiles backup directory
+files="vimrc vim nethackrc gitconfig gitignore"   	# list of files/folders to symlink in homedir
 
 ##########
 
